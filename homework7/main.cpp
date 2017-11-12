3911b2552c854a06148765fe49647976a8edbfce
ZakolM <zakolesnik.m@gmail.com>
2017-11-12 03:52:27 +0300

    homework7, edit main

--- homework7/main.cpp
+++ homework7/main.cpp
@@ -9,6 +9,11 @@ using namespace std;
 template <class T>
 void clean_all(size_t count_vectors, Vector<T> **v, int num, ...)
 {
+    for (size_t i = 0; i < count_vectors; ++i)
+    {
+        delete *(v + i);
+    }
+    delete[] v;
     va_list list;
     va_start(list, num);
     size_t *next;
@@ -18,6 +23,147 @@ void clean_all(size_t count_vectors, Vector<T> **v, int num, ...)
         delete[] next;
     }
     va_end(list);
+}
+
+template <class T>
+void input_vectors(size_t count_vectors, Vector<T> **v, size_t *pa)
+{
+    for (size_t i = 0; i < count_vectors; ++i)
+    {
+        cout << "Введите размер " << i + 1 << " вектора: ";
+        size_t count_el = 0;
+        cin >> count_el;
+        if (!cin || !count_el)
+        {
+            clean_all(0, v, 1, pa);
+            throw ERROR_INPUT;
+        }
+        *(pa + i) = count_el;
+    }
+    for (size_t i = 0; i < count_vectors; ++i)
+    {
+        Vector<int> *v_tmp = new Vector<int>(*(pa + i));
+        if (!pa)
+        {
+            clean_all(i, v, 1, pa);
+            throw bad_alloc();
+        }
+        cout << "Введите " << *(pa + i) << " координаты " << i + 1 << " вектора: ";
+        cin >> *v_tmp;
+        if (!cin)
+        {
+            clean_all(i, v, 1, pa);
+            throw ERROR_INPUT;
+        }
+        v[i] = v_tmp;//Vector<int>(count_el);
+    }
+}
+
+template <class T>
+void all_vectors(size_t count_vectors, Vector<T> **v)
+{
+    cout << endl;
+    cout << "Вывод векторов с их длиннами:" << endl;
+    for (size_t i = 0; i < count_vectors; ++i)
+    {
+        cout << "вектор " << i + 1 << ": " << *v[i] << endl;
+        cout << "длина: " << (*v[i]).len() << endl;
+    }
+}
+
+template <class T>
+void demo_inc_dec(Vector<T> **v)
+{
+    cout << endl;
+    cout << "Демонстрация инкремента и декремента: " << endl;
+    cout << "Изначальный вектор: " << **v << endl;  
+    cout << "Увеличим вектор(инкремент): " << (**v)++ << endl;
+    cout << "Получившийся вектор: " << **v << endl;
+    cout << "Увеличим вектор(декремент теперь)" << ++(**(v)) << endl;
+    cout << "Уменьшим вектор(инкремент): " << (**v)-- << endl;
+    cout << "Получившийся вектор: " << **v << endl;
+    cout << "Уменьшим вектор(декремент теперь)" << --(**(v)) << endl;
+}
+
+template <class T>
+void demo_index(size_t count_vectors, Vector<T> **v, size_t *pa)
+{
+    cout << endl;
+    cout << "Демонстрация индексирования: " << endl;
+    bool flag = true;
+    for (size_t i = 0; i < count_vectors; ++i)
+    {
+        if (*(pa + i) > 1)
+        {
+            cout << "Изначальный вектор: " << *v[i] << endl;
+            cout << "Первый эл-нт: " << (*v[i])[0] << endl;
+            cout << "Последний эл-нт: " << (*v[i])[*(pa + i) - 1] << endl;
+            flag = false;
+            break;
+        }
+    }
+    if (flag)
+    {
+        cout << "Нет вектора размера больше чем 1, невозможно показать индексирование." << endl;
+    }
+}
+
+template <class T>
+void demo_add_minus_vectors(size_t count_vectors, Vector<T> **v, const size_t *pa, size_t *index_i, size_t *index_j, size_t *count)
+{
+    *count = 0;
+    cout << endl;
+    cout << "Демонстрация сложения и вычитания векторов: " << endl;
+    for (size_t i = 0; i < count_vectors / 2 + count_vectors % 2; ++i)
+    {
+        for (size_t j = i + 1; j < count_vectors; ++j)
+        {
+            if (*(pa + i) == *(pa + j))
+            {
+                cout << "Вектор a = {" << **(v + i) << "}\n" << "Сложим с\nВектором b = {" << **(v + j) << "}" << endl;
+                cout << "Получим вектор c = {" << ((**(v + i)) + (**(v + j))) << "}" << endl;
+                cout << "Вычтем из\nВектора a = {" << **(v + i) << "}\nВектор b = {" << **(v + j) << "}" << endl;
+                cout << "Получим вектор c = {" << ((**(v + i)) - (**(v + j))) << "}" << endl;
+                *(index_i + *count) = i; *(index_j + *count) = j;
+                (*count)++;
+                cout << endl;
+            }
+        }
+    }
+    if (!count)
+    {
+        cout << "Нет векторов одинаковых размеров, невозможно показать операции сложения и вычитания." << endl;
+    }
+}
+
+template <class T>
+void demo_mult_VectConst(Vector<T> **v)
+{
+    cout << endl;
+    cout << "Демонстрация умножения вектора на число: " << endl;
+    cout << "Изначальный вектор: " << **v << endl;
+    cout << "Умножим на 2: " << (**v) * 2 << endl;
+}
+
+template <class T>
+void demo_diff_vectors(Vector<T> **v, size_t count, size_t *index_i, size_t *index_j)
+{
+    cout << endl;   
+    cout << "Попарно сравниваем вектора:" << endl;
+    if (count)
+    {
+        for (size_t i = 0; i < count; ++i)
+        {
+            cout << "Вектора " << index_i[i] + 1 << " и " << index_j[i] + 1 << endl;
+            orto(**(v + index_i[i]), **(v + index_j[i]));
+            col(**(v + index_i[i]), **(v + index_j[i]));
+            cout << endl;
+        }
+    }
+    else
+    {
+        cout << "Нет векторов одинаковых размеров, невозможно сравнить вектора." << endl;
+    }
 }
 
 int main(void)
@@ -43,78 +189,14 @@ int main(void)
             throw bad_alloc();
         }
         
-        for (size_t i = 0; i < count_vectors; ++i)
-        {
-            cout << "Введите размер " << i + 1 << " вектора: ";
-            size_t count_el = 0;
-            cin >> count_el;
-            if (!cin || !count_el)
-            {
-                //delete[] v;
-                //delete[] pa;
-                clean_all(0, v, 1, pa);
-                throw ERROR_INPUT;
-            }
-            *(pa + i) = count_el;
-        }
-    
-        for (size_t i = 0; i < count_vectors; ++i)
-        {
-            Vector<int> *v_tmp = new Vector<int>(*(pa + i));
-            if (!pa)
-            {
-                clean_all(i, v, 1, pa);
-                throw bad_alloc();
-            }
-            cout << "Введите " << *(pa + i) << " координаты " << i + 1 << " вектора: ";
-            cin >> *v_tmp;
-            if (!cin)
-            {
-                clean_all(i, v, 1, pa);
-                throw ERROR_INPUT;
-            }
-            v[i] = v_tmp;//Vector<int>(count_el);
-        }
-    
-        cout << endl;
-        cout << "Вывод векторов с их длиннами:" << endl;
-        for (size_t i = 0; i < count_vectors; ++i)
-        {
-            cout << "вектор " << i + 1 << ": " << *v[i] << endl;
-            cout << "длина: " << (*v[i]).len() << endl;
-        }
-       
-        cout << endl;
-        cout << "Демонстрация инкремента и декремента: " << endl;
-        cout << "Изначальный вектор: " << **v << endl;  
-        cout << "Увеличим вектор(инкремент): " << (**v)++ << endl;
-        cout << "Получившийся вектор: " << **v << endl;
-        cout << "Увеличим вектор(декремент теперь)" << ++(**(v)) << endl;
-        cout << "Уменьшим вектор(инкремент): " << (**v)-- << endl;
-        cout << "Получившийся вектор: " << **v << endl;
-        cout << "Уменьшим вектор(декремент теперь)" << --(**(v)) << endl;
-
-        cout << endl;
-        cout << "Демонстрация индексирования: " << endl;
-        bool flag = true;
-        for (size_t i = 0; i < count_vectors; ++i)
-        {
-            if (*(pa + i) > 1)
-            {
-                cout << "Изначальный вектор: " << *v[i] << endl;
-                cout << "Первый эл-нт: " << (*v[i])[0] << endl;
-                cout << "Последний эл-нт: " << (*v[i])[*(pa + i) - 1] << endl;
-                flag = false;
-                break;
-            }
-        }
-        if (flag)
-        {
-            cout << "Нет вектора размера больше чем 1, невозможно показать индексирование." << endl;
-        }
+        input_vectors(count_vectors, v, pa);
         
-        cout << endl;
-        cout << "Демонстрация сложения и вычитания векторов: " << endl;
+        all_vectors(count_vectors, v);
+       
+        demo_inc_dec(v);
+
+        demo_index(count_vectors, v, pa);
+        
         size_t *index_i = new size_t[count_vectors];
         if (!index_i)
         {
@@ -128,48 +210,11 @@ int main(void)
             throw bad_alloc();
         }
         size_t count = 0;
-        for (size_t i = 0; i < count_vectors / 2 + count_vectors % 2; ++i)
-        {
-            for (size_t j = i + 1; j < count_vectors; ++j)
-            {
-                if (*(pa + i) == *(pa + j))
-                {
-                    cout << "Вектор a = {" << **(v + i) << "}\n" << "Сложим с\nВектором b = {" << **(v + j) << "}" << endl;
-                    cout << "Получим вектор c = {" << ((**(v + i)) + (**(v + j))) << "}" << endl;
-                    cout << "Вычтем из\nВектора a = {" << **(v + i) << "}\nВектор b = {" << **(v + j) << "}" << endl;
-                    cout << "Получим вектор c = {" << ((**(v + i)) - (**(v + j))) << "}" << endl;
-                    *(index_i + count) = i; *(index_j + count) = j;
-                    count++;
-                    cout << endl;
-                }
-            }
-        }
-        if (!count)
-        {
-            cout << "Нет векторов одинаковых размеров, невозможно показать операции сложения и вычитания." << endl;
-        }
+        demo_add_minus_vectors(count_vectors, v, pa, index_i, index_j, &count);
         
-        cout << endl;
-        cout << "Демонстрация умножения вектора на число: " << endl;
-        cout << "Изначальный вектор: " << **v << endl;
-        cout << "Умножим на 2: " << (**v) * 2 << endl;
+        demo_mult_VectConst(v);
     
-        cout << endl;   
-        cout << "Попарно сравниваем вектора:" << endl;
-        if (count)
-        {
-            for (size_t i = 0; i < count; ++i)
-            {
-                cout << "Вектора " << index_i[i] + 1 << " и " << index_j[i] + 1 << endl;
-                orto(**(v + index_i[i]), **(v + index_j[i]));
-                col(**(v + index_i[i]), **(v + index_j[i]));
-                cout << endl;
-            }
-        }
-        else
-        {
-            cout << "Нет векторов одинаковых размеров, невозможно сравнить вектора." << endl;
-        }
+        demo_diff_vectors(v, count, index_i, index_j);
         
         clean_all(count_vectors, v, 3, pa, index_i, index_j);
     }
